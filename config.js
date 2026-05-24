57afdb8cfd9ffba099a9d9c739dad35ef618f8ba
macbook <macbook@MacBook-AT.local>
2026-05-24 07:21:16 +0500

    add config.js

---
+++ config.js
@@ -0,0 +1,7 @@
+const APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbycKPxw238EMCUcqXOTVYjvsDKeb-nKtQTyrofUFXjKxxVQ438QH-bzRZBp72XTLPHV0g/exec";
+
+const USERS = [
+  { username: "admin",    password: "isomedia2026",  role: "admin"    },
+  { username: "operator", password: "operator2026",  role: "operator" },
+  { username: "viewer",   password: "viewer2026",    role: "viewer"   }
+];
